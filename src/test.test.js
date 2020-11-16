478bcf53d0f783bba8d87d33647c65ef23c1b515
Hanwen Guan <50503501+hanwenguan0@users.noreply.github.com>
2020-11-15 22:21:38 -0800

    Update test.test.js

--- src/test.test.js
+++ src/test.test.js
@@ -1,45 +1,60 @@
+import React from 'react'
+import Enzyme,{shallow} from 'enzyme'
+import favourite from './ReusableComponent/favourite'
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() })   
+
 test("Click change attribute", () => {
   //Verify that the button className changes when user click the button in favourite.js
+    const i = shallow(<favourite></favourite>)
+    i.simulate('click')
+    expect(i.hasClass('btn btn-warning'))
 });
 test("Click change text", () => {
   //Verify that the button text changes when user click the button in favourite.js
-});
-test("Click jump to new tab1", () => {
-  //Verify that a new tab is open when user click "Detail"
-});
-test("Click jump to new tab2", () => {
-  //Verify that a new tab is open when user click "Buy"
+    const i = shallow(<favourite></favourite>)
+    i.simulate('click')
+    expect(i.text('Add to Cart'))
 });
 test("Click jump to new tab3", () => {
   //Verify that a new tab is open when user click "Click for More Details" under APPLICATION
-});
-test("Input", () => {
-  //Verify that input value displayed in searchbox
+    const i = shallow(<favourite></favourite>)
+    i.simulate('Click for More Details')
+    expect(i)
 });
 test("404 Page", () => {
   //Verify that image is displayed when user visit invalid url
-});
-test("Navigation1", () => {
-  //Verify that navbar button "DEVICE FOR SALE" switch color when it clicked"
+    const i = shallow(<favourite></favourite>)
+    i.text('')
+    expect(i)
 });
 test("Navigation2", () => {
   //Verify that navbar button "REPAIR YOUR DEVICE" switch color when it clicked"
+    const i = shallow(<favourite></favourite>)
+    i.simulate('click')
+    expect(i.hasClass('btn btn-warning'))
 });
 test("Navigation3", () => {
   //Verify that navbar button "APPLICATION" switch color when it clicked"
+    const i = shallow(<favourite></favourite>)
+    i.simulate('click')
+    expect(i.hasClass('btn btn-warning'))
 });
 test("Navigation4", () => {
   //Verify that navbar button "RESEARCH" switch color when it clicked"
+    const i = shallow(<favourite></favourite>)
+    i.simulate('click')
+    expect(i.hasClass('btn btn-warning'))
 });
 test("Navigation5", () => {
   //Verify that navbar button "CONTACT US" switch color when it clicked"
+    const i = shallow(<favourite></favourite>)
+    i.simulate('click')
+    expect(i.hasClass('btn btn-warning'))
 });
 test("Fetched data order", () => {
   //Verify that the order of product card is the same as in .json file (By product name)
-});
-test("Dropdown1", () => {
-  //Verify that when user select item under first dropdown that the item will displayed in the dropdown box in REPAIR YOUR DEVICE
-});
-test("Dropdown2", () => {
-  //Verify that when user select item under second dropdown that the item will displayed in the dropdown box REPAIR YOUR DEVICE
+    const i = shallow(<favourite></favourite>)
+    i.simulate('click')
+    expect(i.hasClass('btn btn-warning'))
 });
