56260e11804846ed74f612900437807c3176f026
Hanwen Guan <50503501+hanwenguan0@users.noreply.github.com>
2020-11-18 12:59:42 -0800

    Update server.js

--- src/DeviceforSale/DeviceInfo/server.js
+++ src/DeviceforSale/DeviceInfo/server.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4000;
 
 server.use(middlewares);
-server.use("", router);
+server.use("/shopping", router);
 
 server.listen(port);
